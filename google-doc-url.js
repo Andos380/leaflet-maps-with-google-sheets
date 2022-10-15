67ded0550710fc587fead5777c3cd5e8a17b1c64
Andos <97794781+Andos380@users.noreply.github.com>
2022-10-15 13:38:19 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nvYZYEJucLS4Od08d4qJEPLS4HjvlrLHSG1PESrcU8E/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDpiINfPRxPrDsVJBYpLRpWZ9BjHfM894c';
